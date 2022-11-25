7899747ddf43b7a082984de3cb7260d8a305fac2
Yahaya Bathily <developpeur.yb@gmail.com>
2022-11-25 22:49:13 +0100

    move the example project in another folder

--- project/sabo/interfaces/MaintenanceManagerInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Sabo\Interface;
-
-interface MaintenanceManagerInterface extends ShowableInterface
-{
-	// return if client is allowed to continue in website during maintenance
-	public function can_continue_in_website():bool;
-}
--- project/src/model/interfaces/CondInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Model\Interface;
-
-interface CondInterface
-{
-	public function is_valid(mixed $data):bool;
-	public function get_not_valid_message():string;
-}
